497abfb0096780728080780154bd5f939f97817c
Akashruti <shruti_01@live.com>
2015-03-10 12:44:49 -0400

    Added utility method

---
+++ Comp123_Lesson9_Part1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comp123_Lesson9_Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // UTILITY METHODS
+        private static void WaitForKey()
+        {
+            Console.WriteLine();
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        }
+    }
+}
